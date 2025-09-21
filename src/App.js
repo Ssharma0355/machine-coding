22d0f466df6152b7008a6bf3912c91571a129bc9
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-09-21 08:43:33 +0530

    Intial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TabForm from './solutions/TabForm';
+
+function App() {
+  return (
+   <div>
+    <TabForm />
+
+   </div>
+  );
+}
+
+export default App;
---
+++ src/solutions/TabForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TabForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TabForm
