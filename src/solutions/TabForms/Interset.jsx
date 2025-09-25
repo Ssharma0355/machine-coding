50a135b0c44d9b941dde4436b25fe8e906ccd419
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-09-25 12:40:01 +0530

    intial

--- src/solutions/TabForms/Interset.jsx
+++ src/solutions/TabForms/Interset.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const Interset = () => {
+const Interset = ({data, setData}) => {
+  const {interset} = data
   return (
-    <div>
-      <h1>Interset</h1>
+    <div className='interset-conatiner'>
+      <label htmlFor="">
+        Coding
+        <input type="checkbox" name="coding" checked={interset.includes("coding")} />
+      </label>
+       <label htmlFor="">
+        DSA
+        <input type="checkbox" name="" id="" />
+      </label>
     </div>
   )
 }
